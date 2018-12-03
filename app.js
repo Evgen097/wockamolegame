cb30b606c2ed9fd2413514c180101fddb386c7a1
Evgen097 <k0990245853@gmail.com>
2018-12-03 16:34:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function (req, res) {
+    return res.redirect('/');
+});
+
+app.listen(PORT, function () {
+    console.log(`Listening on ${ PORT }`);
+});
+
+// echo "# wockamolegame" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin https://github.com/Evgen097/wockamolegame.git
+//     git push -u origin master
